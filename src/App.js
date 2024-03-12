45fb21fbae8aec50d46f3247d80c4fd2c5570d90
thehassanshaikh <thehasanshaikh@gmail.com>
2024-03-12 11:02:50 +0530

    Hero-section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import HeroSectoin from './components/HeroSectoin';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <HeroSectoin />
     </div>
   );
 }
---
+++ src/components/HeroSectoin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HeroSectoin() {
+    return (
+        <div className="text-5xl font-extrabold ...">
+        <span className ="text-transparent bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text">
+          Hello world
+        </span>
+      </div>
+    )
+}
+
+export default HeroSectoin
