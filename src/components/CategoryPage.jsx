074fc8250e3b53da89688f6c02834a141ec46d93
Mohammad Hassan shaikh <thehasanshaikh@gmail.com>
2024-03-16 23:23:26 +0530

    Worked on responsivenss on page

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -12,8 +12,8 @@ function Page() {
           {
             [...Array(10)].map((_, index) => (
               <div key={index} className='flex gap-5 border-b border-gray-200 py-3 hover:pl-4 ease-in-out	duration-300'>
-                <div className='bg-gradient-to-r from-pink-500 to-violet-500 rounded'>
-                  <img className='md:p-5' src="https://undesign.learn.uno/images/logos/undraw.png" alt="" />
+                <div className='bg-gradient-to-r from-pink-500 to-violet-500 rounded h-full w-20 md:w-auto md:h-auto'>
+                  <img className='md:p-5 p-2 w-full ' src="https://undesign.learn.uno/images/logos/undraw.png" alt="" />
                 </div>
                 <div className=' flex flex-col justify-center'>
                   <h3 className='font-semibold'>Undraw</h3>
